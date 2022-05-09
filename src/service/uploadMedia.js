942363d49accfffaa654f05360e11b32b370782d
Ngo Hiep <hoanghiephai@gmail.com>
2022-05-09 16:58:23 +0700

    auto resize thumbnail

--- src/service/uploadMedia.js
+++ src/service/uploadMedia.js
@@ -3,20 +3,28 @@ const createHttpError = require("http-errors");
 module.exports = {
   StoreThumbToDB: async (model, modelId, filePath, fileId) => {
     try {
+      const urlEndPoint = filePath.split("/");
+      const finalUrl = [
+        ...urlEndPoint.slice(0, 6),
+        "w_350,h_200,c_scale",
+        ...urlEndPoint.slice(6),
+      ].join("/");
+      console.log(finalUrl);
       await model.updateOne(
         { _id: modelId },
         {
           thumbnail: {
-            url: filePath,
+            url: finalUrl,
             id: fileId,
           },
         }
       );
 
       return {
-        url: filePath,
+        url: finalUrl,
         id: fileId,
       };
+
     } catch (error) {
       throw new createHttpError(500, error.message);
     }
@@ -26,16 +34,16 @@ module.exports = {
       const newVideos = videoArr.map((vid) => {
         const url = vid.path,
           id = vid.filename.split("/").at(-1),
-          name = vid.originalname.split('.')[0];
+          name = vid.originalname.split(".")[0];
 
         return {
           url,
           id,
-          name
+          name,
         };
       });
 
-    //   console.log(newVideos);
+      //   console.log(newVideos);
 
       await model.findByIdAndUpdate(modelId, {
         $push: { videos: { $each: newVideos } },
